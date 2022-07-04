fdf4e90a9ba7c4888df05701e8631ae22080ffd6
Etienne Dauvergne <contact@ekyna.com>
2022-07-04 09:59:22 +0200

    [SocialButtons] PHP 8.1

--- Service/SettingSchema.php
+++ Service/SettingSchema.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Ekyna\Bundle\SocialButtonsBundle\Service;
 
 use Ekyna\Bundle\SettingBundle\Schema\AbstractSchema;
-use Ekyna\Bundle\SettingBundle\Schema\SettingsBuilder;
+use Ekyna\Bundle\SettingBundle\Schema\SettingBuilder;
 use Ekyna\Bundle\SocialButtonsBundle\Form\Type\LinkType;
 use Ekyna\Bundle\UiBundle\Form\Type\CollectionType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -20,7 +20,7 @@ use function Symfony\Component\Translation\t;
  */
 class SettingSchema extends AbstractSchema
 {
-    public function buildSettings(SettingsBuilder $builder): void
+    public function buildSettings(SettingBuilder $builder): void
     {
         $builder
             ->setDefaults(array_merge([
